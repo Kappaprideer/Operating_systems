4e30d1aaf9eea8a097f6295d507cd0e993fd9d87
Kappaprideer <azab@student.agh.edu.pl>
2023-03-16 21:28:46 +0100

    zad1 in progress

---
+++ cw02/zad1/homemade_tr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+
+int main(int arg, char** args){
+    if(arg!=5){
+        fprintf(stderr, "Invalid count of arguments!\n");
+        return 1;
+    }
+
+    FILE* source;
+    
+    
+    
+
+    
+    return 0;
+}
