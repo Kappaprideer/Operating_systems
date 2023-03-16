e65e0ccba52bf96817f9829ca94f7ac9104e3cfe
Kappaprideer <azab@student.agh.edu.pl>
2023-03-16 22:43:33 +0100

    half of zad1 done

--- cw02/zad1/homemade_tr.c
+++ cw02/zad1/homemade_tr.c
@@ -5,6 +5,44 @@
 #include <time.h>
 #include <string.h>
 
+#define BLOCK_SIZE 1024
+
+long long size_of_file(FILE* file){
+    long long file_size;
+    fseek(file, 0, SEEK_END);
+    file_size =  ftell(file);
+    fseek(file, 0, SEEK_SET);
+    return file_size;
+}
+
+void library_function(int arg, char** args){
+    FILE *source, *destiny;
+    char argument, change_to;
+    char* buffer;
+    argument = args[1][0];
+    change_to = args[2][0];
+
+    source = fopen(args[3],"rb");
+    long long file_size = size_of_file(source);
+    buffer = calloc(sizeof(char), file_size);
+    size_t index = 0 ;
+    while (index<file_size) {
+        size_t bytes_to_read = (file_size-index < BLOCK_SIZE) ? file_size-index : BLOCK_SIZE;
+        index += fread(&buffer[index], sizeof(char), bytes_to_read, source);
+    }
+    for(long long index=0; index<file_size; index++){
+        if(buffer[index] == argument){
+            buffer[index] = change_to;
+        }
+    }
+    destiny = fopen(args[4], "wb");
+    fwrite(buffer,sizeof(char), file_size,destiny);
+    free(buffer);
+    fclose(destiny);
+    fclose(source);
+}
+
+
 
 
 int main(int arg, char** args){
@@ -13,10 +51,9 @@ int main(int arg, char** args){
         return 1;
     }
 
-    FILE* source;
-    
-    
-    
+
+
+
 
     
     return 0;
