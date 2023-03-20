68f2e41e3a6de68b106da8dc20c71b960b8356a1
Kappaprideer <azab@student.agh.edu.pl>
2023-03-20 21:16:44 +0100

    zad2 done

---
+++ cw03/zad2/path_ls.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int arg, char** args){
+    if(arg != 2){
+        fprintf(stderr, "Wrong number of arguments!\n");
+        return EXIT_FAILURE;
+    }
+
+    char* program_name = malloc(sizeof(char)*(strlen(args[0])-2));
+    
+    strncpy(program_name, args[0]+2, strlen(args[0])-2);
+    
+    printf("%s ", program_name);
+    
+    free(program_name);
+
+    fflush(stdout);
+
+    execl("/bin/ls", "/bin/ls", args[1], NULL);
+
+    return EXIT_SUCCESS;
+}
